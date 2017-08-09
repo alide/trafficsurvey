c3bfcaaf065f404f15844bf29ad6286778b131fd
Linh Tran <gplinh@gmail.com>
2017-08-09 11:05:08 +1000

    Changed Logo

--- js/main.js
+++ js/main.js
@@ -19,14 +19,14 @@ function main() {
 
 	
     // Show Menu on Book
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 600;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
+    // $(window).bind('scroll', function() {
+    //     var navHeight = $(window).height() - 600;
+    //     if ($(window).scrollTop() > navHeight) {
+    //         $('.navbar-default').addClass('on');
+    //     } else {
+    //         $('.navbar-default').removeClass('on');
+    //     }
+    // });
 
     $('body').scrollspy({ 
         target: '.navbar-default',
