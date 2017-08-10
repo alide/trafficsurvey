7762e03d01a2c13c59f7986c8884df444b2c364d
Linh Tran <gplinh@gmail.com>
2017-08-10 15:00:58 +1000

    Animation added

--- js/main.js
+++ js/main.js
@@ -53,4 +53,69 @@ function main() {
 
 
 }
+
 main();
+
+$(function(){ 
+	// Animation
+
+    // Init
+    var controller = new ScrollMagic.Controller();
+    var tl = new TimelineMax();
+
+    
+    // Animate target
+    var sceneTarget = new ScrollMagic.Scene()
+      .triggerElement('#target')
+      .setClassToggle('#target', 'focusing')
+      .addTo(controller);
+    
+    // Animate Intro3 list
+    var listItems = $('#intro3 ul.checklist li');
+    tl.staggerFrom(listItems, 0.4, {x: -50, opacity:0, ease: Cubic.easeInOut}, 0.2);
+
+    var sceneStrength = new ScrollMagic.Scene()
+      .triggerElement("#intro3")
+      .setTween(tl)
+      .addTo(controller);
+
+    // Animate About list
+    var listItems = $('#about ul.checklist li');
+    var text = $('#about p');
+    var tl2 = new TimelineMax()
+      .from(text, 0.6, {opacity: 0, y: -50})
+      .staggerFrom(listItems, 0.6, {x: 50, opacity:0, ease: Power4.easeOut}, 0.8);
+
+    var sceneAbout = new ScrollMagic.Scene()
+      .triggerElement("#about")
+      .setTween(tl2)
+      .addTo(controller);
+
+    // Services
+    var serviceDesc = $('#services .service .desc');
+    var etc = $('#services .etc');
+    var icons = $('#services .icon-wrapper');
+    var tl3 = new TimelineMax()
+      .staggerFrom(icons, 0.4, {y: -10, opacity:0, ease: Cubic.easeInOut}, 0.2)
+      .staggerFrom(serviceDesc, 0.6, {x: -100, opacity:0, scale: 1.25, ease: Cubic.easeInOut}, 0.2)
+      .from(etc, 0.6, {y: -20, opacity:0, ease: Cubic.easeInOut})
+
+    var sceneServices = new ScrollMagic.Scene()
+      .triggerElement("#services")
+      .setTween(tl3)
+      .addTo(controller);
+
+
+    // Animate Contact section
+    var markers = $('#contact .fa');
+    var tl4 = new TimelineMax()
+      .staggerFromTo(markers, 0.3,
+        {y: -20, opacity: 0, scale: 1.75},
+        {y: 0, opacity: 1, scale: 1, color: "#99ff9e"},
+        0.3, "+=0.1");
+
+    var sceneContact = new ScrollMagic.Scene()
+      .triggerElement('#contact')
+      .setTween(tl4)
+      .addTo(controller);
+});
